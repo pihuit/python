d930f7de0ddef12c786b06f009ad25aebdd6297e
root <root@station176.example.com>
2018-05-26 11:27:59 +0530

    else if program

---
+++ elif.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print "press 1:"
+print "press 2:"
+ch=raw_input("enter something")
+if ch=='1':
+	print "you entered 1"
+elif ch=='2':
+	print "you entered 2"
+else:
+	print "no chance!!!"
+				
